836ad70075ae5a3366b2e970a5fe2ecc8a42c069
Cristian Bica <cristian.bica@gmail.com>
2016-06-05 09:46:39 +0300

    Fix handling messages

--- lib/lita/extensions/wizard.rb
+++ lib/lita/extensions/wizard.rb
@@ -2,16 +2,23 @@ module Lita
   module Extensions
     class Wizard
       def self.call(payload)
+        Lita.logger.debug "LEW: #{payload.inspect}"
         message = payload[:message]
         route = payload[:route]
         robot = payload[:robot]
+
+        # mark message as processed and return next time
         return true if message.extensions[:processed_by_wizard]
-        return true unless message.private_message?
         message.extensions[:processed_by_wizard] = true
-        handled = Lita::Wizard.handle_message(robot, message)
-        return true if handled
-        dummy = (route.extensions[:dummy] == true)
-        !dummy
+
+        # if private messages and user has a pending wizard handle the message
+        if message.private_message? && Lita::Wizard.pending_wizard?(message.user.id)
+          handled = Lita::Wizard.handle_message(robot, message)
+          return true if handled
+        end
+
+        # return
+        !(route.extensions[:dummy] == true)
       end
 
       Lita.register_hook(:validate_route, self)
